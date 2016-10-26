2a2c21d8345a6e668dfd0a6fd10fd2c8a377f0c2
James Graham <james@hoppipolla.co.uk>
2016-10-26 23:13:03 +0100

    Autoclassify through the TextLogError rather than through the FailureLine

--- tests/autoclassify/utils.py
+++ tests/autoclassify/utils.py
@@ -11,7 +11,17 @@ from treeherder.model.search import refresh_all
 test_line = {"action": "test_result", "test": "test1", "subtest": "subtest1",
              "status": "FAIL", "expected": "PASS", "message": "message1"}
 log_line = {"action": "log", "level": "ERROR", "message": "message1"}
-crash_line = {"action": "crash", "signature": "signature"}
+crash_line = {"action": "crash", "signature": "signature", "test": "test1"}
+
+
+def create_lines(test_job, lines):
+    error_lines = create_text_log_errors(test_job, lines)
+    failure_lines = create_failure_lines(test_job, lines)
+
+    for error_line, failure_line in zip(error_lines, failure_lines):
+        error_line.failure_line = failure_line
+        error_line.save()
+    return error_lines, failure_lines
 
 
 def create_failure_lines(job, failure_line_list,
@@ -46,6 +56,8 @@ def get_data(base_data, updates):
     elif data["action"] == "log":
         if data["level"] not in ("ERROR", "CRITICAL"):
             return
+    elif data["action"] == "crash":
+        pass
     else:
         return
     return data
